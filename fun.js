b361b5c5931e3b4cd7d5cb9a939fa34ec6379ad1
Wei Sun <jacksunwei@gmail.com>
2015-06-03 16:32:38 +0800

    modify interface of iterator

--- fun.js
+++ fun.js
@@ -4,49 +4,44 @@
  * lazy, functional programes
  */
 
-/**
- * 
- */
-
-/**
- * Iterator: 
- * @interface: IteratorResult next() 
- *             throw an error if cannot go next
- */
-
-/**
- * IteratorResult: 
- * var iterResult = {
- *   "value": <value>,
- *   "hasNext": <boolean>
- * }
- */
-
 var funjs = {};
 
 (function(funjs) {
   
+  var LazyIterator = function(gen, value, param, state) {
+    this._gen = gen;
+    this._value = value;
+    this._param = param;
+    this._state = state;
+  };
+  
+  LazyIterator.prototype.next = function() {
+    return this._gen(this._param, this._state);
+  };
+  
+  LazyIterator.prototype.getValue = function() {
+    return this._value(this._param, this._state);
+  };
+  
   funjs.iter_wrapper = function(gen, value, param, state) {
-    return {
-      "gen": gen, // return a iterator contains next value
-      "param": param, // param 
-      "state": state, // state
-      "value": value, // return the value of iterator
-      "next": function() { // public interface to iterate next
-        return this.gen(param, state);
-      }, 
-      "getValue": function() { // public interface to get value
-        return this.value(param, state);
-      },
-    };
+    return new LazyIterator(gen, value, param, state);
   };
   
   funjs.nil_iter = funjs.iter_wrapper(null, function () {
     return null;
   }, null, null);
   
+  funjs.to_array = function(iter) {
+    var res = [];
+    while (iter != funjs.nil_iter) {
+      res.push(iter.getValue());
+      iter = iter.next();
+    }
+    return res;
+  };
+  
   funjs.array_iter = function(arr) {
-    var value_func = function(param, state) {
+    var array_iter_value = function(param, state) {
       return param[state];
     };
     
@@ -54,48 +49,44 @@ var funjs = {};
       if (1 + state == param.length) {
         return funjs.nil_iter;
       } else {
-        return funjs.iter_wrapper(gen_func, value_func, param, state + 1);
+        return funjs.iter_wrapper(gen_func, array_iter_value, param, state + 1);
       }
     };
     
-    return funjs.iter_wrapper(gen_func, value_func, arr, 0);
-  };
-  
-  funjs.map_iter = function(transform, iter) {
-    var value_func = function(param, state) {
-      var v = transform(iter.value(param, state));
-      return v;
-    };
-    
-    var map_iter_gen = function(param, state) {
-      var next_iter = iter.gen(param, state);
-      if (next_iter == funjs.nil_iter) {
-        return next_iter;
-      } else {
-        return funjs.iter_wrapper(map_iter_gen, value_func, next_iter.param, next_iter.state);
-      }
-    };
-    
-    return funjs.iter_wrapper(map_iter_gen, value_func, iter.param, iter.state);
+    return funjs.iter_wrapper(gen_func, array_iter_value, arr, 0);
   };
   
   funjs.map = function(transform, list) {
+    var iter = list;
     if (Array.isArray(list)) {
-      return funjs.map_iter(transform, funjs.array_iter(list));
+      iter = funjs.array_iter(list);
     }
+
+    var map_iter_value = function(param, state) {
+      return transform(iter._value(param, state));
+    };
+    
+    var map_iter_gen = function(param, state) {
+      var next_iter = iter._gen(param, state);
+      if (next_iter == funjs.nil_iter) {
+        return next_iter;
+      } else {
+        return funjs.iter_wrapper(map_iter_gen, map_iter_value, next_iter._param, next_iter._state);
+      }
+    };
+    
+    return funjs.iter_wrapper(map_iter_gen, map_iter_value, iter._param, iter._state);
   };
   
 }(funjs));
 
 //console.log(funjs);
 
-var iter = funjs.map(function(v) { return v + 1; }, [1, 2, 3, 4]);
+var array = [1, 2, 3, 4];
 
-var res = [];
-while (iter != funjs.nil_iter) {
-  res.push(iter.getValue());
-  iter = iter.next();
-}
+var arr_iter = funjs.array_iter(array);
+console.log('arr', funjs.to_array(arr_iter));
 
-console.log(iter);
-console.log('res', res);
+
+var map_iter = funjs.map(function(v) { return v + 1; }, array);
+console.log(array, ' -> ', funjs.to_array(map_iter));
